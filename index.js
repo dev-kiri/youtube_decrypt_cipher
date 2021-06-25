881b923dc8575418f51ea4ced85688d00c9d757d
dev-kiri <72954146+dev-kiri@users.noreply.github.com>
2021-06-25 12:02:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // 2021-06-25 dev-kiri
+// 그새 또 코드가 바꼈네;;
 
 /**
  * Decrypts encrypted cipher signature.
@@ -6,16 +7,16 @@
  * @returns {string} decrypted signature
  */
 function decrypt(signature) {
-    const pB = {
-        vg: function (sig, depth) {
+    const nB = {
+        UE: function (sig, depth) {
             sig.reverse();
             return sig;
         },
-        fl: function (sig, depth) {
+        Cz: function (sig, depth) {
             [sig[0], sig[depth % sig.length]] = [sig[depth % sig.length], sig[0]];
             return sig;
         },
-        IF: function (sig, depth) {
+        mx: function (sig, depth) {
             sig.splice(0, depth);
             return sig;
         }
@@ -23,9 +24,10 @@ function decrypt(signature) {
     
     const sig = [...signature];
     
-    pB.vg(sig, 47);
-    pB.fl(sig, 45);
-    pB.fl(sig, 32);
+    nB.UE(a, 19); // UE (reverse)
+    nB.Cz(a, 15); // Cz (swap)
+    nB.mx(a, 3); // mx (splice)
+    nB.Cz(a, 17); // Cz (swap)
     
     return sig.join("");
 }
